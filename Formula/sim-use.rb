6c9bdc10730bb75d8de2a388a9e1815ba0321ea0
onevcat <wei.wang@linecorp.com>
2026-07-29 14:28:58 +0900

    sim-use v0.12.0

--- Formula/sim-use.rb
+++ Formula/sim-use.rb
@@ -2,22 +2,18 @@ class SimUse < Formula
   desc "Give AI agents eyes and hands on iOS Simulator and Android devices"
   homepage "https://github.com/lycorp-jp/sim-use"
   license "Apache-2.0"
-  version "0.11.0"
+  version "0.12.0"
   depends_on macos: :sonoma
 
-  url "https://github.com/lycorp-jp/sim-use/releases/download/v0.11.0/sim-use-v0.11.0.tar.gz"
-  sha256 "4bad411e9c05233391ad06ff35523dbaf74868f42a8005b9de6c4ff6e96f5208"
+  url "https://github.com/lycorp-jp/sim-use/releases/download/v0.12.0/sim-use-v0.12.0.tar.gz"
+  sha256 "fb452afe7ff56fba314c693dcd7c149ebd2831dd93f6590b9fa30f6f695cdaa4"
 
   def install
-    libexec.install "sim-use", "Frameworks", "SimUse_SimUse.bundle", "SimUse_AndroidBackend.bundle"
+    libexec.install "sim-use", "SimUse_SimUse.bundle", "SimUse_AndroidBackend.bundle"
     bin.write_exec_script libexec/"sim-use"
   end
 
   def post_install
-    Dir.glob("#{libexec}/Frameworks/*.framework").each do |framework|
-      system "codesign", "--force", "--sign", "-", "--timestamp=none", framework
-    end
-
     system "codesign", "--force", "--sign", "-", "--timestamp=none", libexec/"sim-use"
   end
 
