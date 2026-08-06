094605c40c942db1b9428506b26228a8b405949b
onevcat <wei.wang@linecorp.com>
2026-08-06 19:15:42 +0900

    sim-use v0.13.0

--- Formula/sim-use.rb
+++ Formula/sim-use.rb
@@ -2,11 +2,11 @@ class SimUse < Formula
   desc "Give AI agents eyes and hands on iOS Simulator and Android devices"
   homepage "https://github.com/lycorp-jp/sim-use"
   license "Apache-2.0"
-  version "0.12.0"
+  version "0.13.0"
   depends_on macos: :sonoma
 
-  url "https://github.com/lycorp-jp/sim-use/releases/download/v0.12.0/sim-use-v0.12.0.tar.gz"
-  sha256 "fb452afe7ff56fba314c693dcd7c149ebd2831dd93f6590b9fa30f6f695cdaa4"
+  url "https://github.com/lycorp-jp/sim-use/releases/download/v0.13.0/sim-use-v0.13.0.tar.gz"
+  sha256 "f3da237663199faf4c7f6796c3ede6ce7583da872dc27d92eb3b7eb051725081"
 
   def install
     libexec.install "sim-use", "SimUse_SimUse.bundle", "SimUse_AndroidBackend.bundle"
